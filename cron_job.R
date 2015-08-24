468e1044fd58fed435b2ab75eacb7f37bec81a0b
lassefolkersen <lassefolkersen@gmail.com>
2015-08-24 10:04:34 +0200

    s

--- cron_job.R
+++ cron_job.R
@@ -18,25 +18,34 @@ foldersToCheck<-grep("^imputation_folder",s,value=T)
 
 
 for(folderToCheck in foldersToCheck){
-	setwd(paste("/home/ubuntu/imputations/",folderToCheck))	
+	setwd(paste("/home/ubuntu/imputations/",folderToCheck,sep=""))	
 	
 	if(!file.exists("job_status.txt")){
 		print(paste("Didn't find a job-status file - should probably auto-delete",folderToCheck))
 		next
 	}
 	
-	jobStatus<-read.table("job_status.txt",stringsAsFactors=FALSE,header=FALSE)
+	jobStatus<-read.table("job_status.txt",stringsAsFactors=FALSE,header=FALSE,sep="\t")[1,1]
+	
+	
 	if(jobStatus=="Job is not ready yet"){
-		print("Found job-status file - but job is not ready yet",folderToCheck)		
+		print(paste("Found job-status file - but job is not ready yet",folderToCheck))
+		next
+	}
+	
+	if(jobStatus=="Job is running"){
+		print(paste("Found job-status file - but job is already running",folderToCheck))
 		next
 	}
 	
 
-	
-	
 	if(jobStatus=="Job is ready"){
-		print("Found job-status file - and job is ready",folderToCheck)		
-		load("imputation_commands.rdata")
+		print(paste("Found job-status file - and job is ready",folderToCheck)		)
+
+		unlink("job_status.txt")
+		write.table("Job is running",file="job_status.txt",col.names=F,row.names=F,quote=F)
+
+		load("imputation_commands.rdata")		
 		
 		for(i in 1:length(cmd2)){
 			print(paste("running cmd",i,"of",length(cmd2)))
@@ -46,23 +55,23 @@ for(folderToCheck in foldersToCheck){
 		
 
 		print("Zipping files")
-		outputFiles<-grep("[1-9]\\.gen$",list.files(homeFolder,full.names=T))
-		zipFileOut<-paste(homeFolder,paste(uniqueID,".zip",sep=""),sep="/")
+		outputFiles<-grep("[1-9]\\.gen$",list.files(),value=T)
+		zipFileOut<-paste("/home/ubuntu/imputations",folderToCheck,paste(uniqueID,".zip",sep=""),sep="/")
 		zip(zipFileOut, outputFiles, flags = "-r9X", extras = "",zip = Sys.getenv("R_ZIPCMD", "zip"))
 		
 		
 		print("Moving zip files to download location and clean up")
-		finalLocation <- paste("/srv/shiny-server/",zipFileOut,sep="")
-		cmd3 <- system("sudo mv", zipFileOut, finalLocation)
+		finalLocation <- paste("/srv/shiny-server/",basename(zipFileOut),sep="")
+		cmd3 <- paste("sudo mv", zipFileOut, finalLocation)
 		system(cmd3,intern=T)
 		setwd("/home/ubuntu/imputations/")
-		unlink(homeFolder,recursive = TRUE)
+		unlink(folderToCheck,recursive = TRUE)
 		
 		print("Getting IP and sending mail")
 		ip<-sub("\"}$","",sub("^.+\"ip\":\"","",readLines("http://api.hostip.info/get_json.php", warn=F)))
-		location <- paste(ip,basename(fileOut),sep="/")
+		location <- paste(ip,basename(finalLocation),sep="/")
 		message <- paste("For the next 24 hours you can retrieve your imputed genome at this address:\n",location)
-		sendmail(recipient=to, subject=subject, message=message, password="rmail")
+		sendmail(recipient=email, subject="Imputation is ready", message=message, password="rmail")
 		
 		print("Wait 24 hours")
 		Sys.sleep(24*60*60)
