3ab92e33072a351c5d069ed8b3a7b3715c193fb9
lassefolkersen <lassefolkersen@gmail.com>
2015-08-31 15:09:49 +0200

    s

--- imputeme/cron_job.R
+++ imputeme/cron_job.R
@@ -3,9 +3,9 @@
 #Strategy - setup this to run every hour on the hour, 
 # 	
 # sudo crontab -u root -e
-# 50 * * * * Rscript /srv/shiny-server/imputeme/cron_job.R > /var/log/cron_log
+# 50 * * * * Rscript /srv/shiny-server/gene-surfer/imputeme/cron_job.R > /var/log/cron_log
 
-source("/srv/shiny-server/imputeme/functions.R")
+source("/srv/shiny-server/imputeme/gene-surfer/functions.R")
 
 
 
