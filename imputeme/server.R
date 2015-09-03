fa55cb43967728fc4aeb9f7f9cbf08a67dd31888
lassefolkersen <lassefolkersen@gmail.com>
2015-09-03 11:44:37 +0200

    stop message

--- imputeme/server.R
+++ imputeme/server.R
@@ -40,6 +40,7 @@ shinyServer(function(input, output) {
 			email <- isolate(input$email)
 			filename <- isolate(input$largeFile[["name"]])
 			if(is.null(path))return("No file selected")
+			stop("The servere was temporarily moved to an amazon-EC2-'small' machine, which can't handle the actual imputation. The amazon-EC2-'large' which can, quickly be set up again, but it's too expensive to leave running. Write me an email at lassefolkersen@gmail.com if I should start it")
 			out<-prepare_23andme_genome(path,email,filename)
 			return(out)
 		}
