80816369d500a0a0821e40535879d33d1dd2d756
lassefolkersen <lassefolkersen@gmail.com>
2015-09-25 11:04:27 +0200

    s

--- imputeme/ui.R
+++ imputeme/ui.R
@@ -5,7 +5,7 @@ shinyUI(fluidPage(
 	titlePanel("Analyse genome"),
 	sidebarLayout(
 		sidebarPanel(
-			fileInput("largeFile", "Upload 23andme data", multiple = FALSE, accept = NULL),
+			fileInput("largeFile", "Upload genome data", multiple = FALSE, accept = NULL),
 			textInput(inputId="email", label = "Email", value = "lassefolkersen@gmail.com"),
 			a("Terms of use", href="terms_of_use.html"),
 			# p("Then start imputation. This will take a while, but we'll mail you a download-link when ready"),
