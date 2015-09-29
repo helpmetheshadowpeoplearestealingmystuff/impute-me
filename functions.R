b891502114f3e2ea17522a9c88d1557e963bebcd
lassefolkersen <lassefolkersen@gmail.com>
2015-09-29 12:26:42 +0200

    creating a pData-overview function

--- functions.R
+++ functions.R
@@ -645,3 +645,42 @@ crawl_for_snps_to_analyze<-function(uniqueIDs=NULL){
 # 		system(cmd1)
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+make_overview_of_samples<-function(verbose=T){
+	uniqueIDs<-list.files("/home/ubuntu/data/")
+	all_pData<-list()
+	for(uniqueID in uniqueIDs){
+		pDataFile<-paste("/home/ubuntu/data/",uniqueID,"/pData.txt",sep="")
+		if(file.exists(pDataFile)){
+		all_pData[[uniqueID]]<-read.table(pDataFile,header=T,stringsAsFactors=F)
+		}else{
+			if(verbose)print(paste("Didn't find a pData file for",uniqueID))	
+		}
+	}
+	all_columns<-unique(unlist(lapply(all_pData,colnames)))
+	pData<-as.data.frame(matrix(nrow=0,ncol=length(all_columns),dimnames=list(NULL,all_columns)))
+	for(uniqueID in names(all_pData)){
+		p<-all_pData[[uniqueID]]
+		for(missing_col in all_columns[!all_columns%in%colnames(p)]){
+			p[1,missing_col]<-NA
+		}
+		pData<-rbind(pData,p[,all_columns])
+	}
+	rownames(pData)<-pData[,"uniqueID"]
+	return(pData)
+	
+	
+}
