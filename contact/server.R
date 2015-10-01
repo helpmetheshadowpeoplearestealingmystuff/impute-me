5170c8f34a6924a7283f4e7708d95f0f576ac7f3
lassefolkersen <lassefolkersen@gmail.com>
2015-10-01 13:24:48 +0200

    Adding contact page

---
+++ contact/server.R
@@ -0,0 +1,18 @@
+library("shiny")
+
+
+shinyServer(function(input, output) {
+	
+	
+	
+	output$text1 <- renderText({ 
+		
+		m<-"This page is created and operated by <a href='http://www.cbs.dtu.dk/staff/show-staff.php?id=1202'>Lasse Folkersen</a>.<br><br>Feel free to write me an <a href='http://www.google.com/recaptcha/mailhide/d?k=01pdzWyCfeU-_1PRAPdKlJfg==&amp;c=3eyQPG-VqkHu6ECGRBSHdRraKCXOUsVtLpuyWWt-dpY=' onclick='window.open('http://www.google.com/recaptcha/mailhide/d?k\07501pdzWyCfeU-_1PRAPdKlJfg\75\75\46c\0753eyQPG-VqkHu6ECGRBSHdRraKCXOUsVtLpuyWWt-dpY\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;' title='Reveal this e-mail address'>email</a>."
+		return(m)
+	})
+	
+	
+	
+})
+
+
