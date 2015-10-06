8ca0725c7764b0656b98a981820773983e022d83
lassefolkersen <lassefolkersen@gmail.com>
2015-10-06 07:20:58 -0400

    forgot to create output dir

--- imputeme/imputation_cron_job.R
+++ imputeme/imputation_cron_job.R
@@ -104,6 +104,11 @@ setwd(runDir)
 load(paste(runDir,"/variables.rdata",sep=""))
 rawdata<-paste(runDir,"/",uniqueID,"_raw_data.txt",sep="")
 
+#if running as node, we also create the output dir already here
+if(serverRole== "Node"){
+	dir.create("/home/ubuntu/data/",uniqueID,sep="")
+}
+
 #run the imputation
 run_imputation(rawdata=rawdata, runDir=runDir)
 
