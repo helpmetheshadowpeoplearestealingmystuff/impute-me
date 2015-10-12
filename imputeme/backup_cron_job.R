bc582b8c40ed844f5dc4452811cd379a1e20be92
lassefolkersen <lassefolkersen@gmail.com>
2015-10-12 17:44:26 -0400

    Don't do the backup (in preparation for timed-delete function)

--- imputeme/backup_cron_job.R
+++
@@ -1,7 +0,0 @@
-
-
-# sudo crontab -u ubuntu -e
-# 10 20 * * * /srv/shiny-server/gene-surfer/imputeme/backup_cron_job.R > /home/ubuntu/misc_files/cron_logs/`date +\%Y\%m\%d\%H\%M\%S`-backup-cron.log 2>&1
-
-
-rsync -r -h -t --progress /home/ubuntu/data/ lasfol@computerome.cbs.dtu.dk:/home/people/lasfol/dataBulk/Miscellanous/2015-10-05_impute_me_backup/
