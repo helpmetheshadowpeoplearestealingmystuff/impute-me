2e2b59777bdd26ad07d3b16c9d2e98d9c7dd8fdc
lassefolkersen <lassefolkersen@gmail.com>
2015-10-14 15:00:18 +0200

    more text update

--- BRCA/ui.R
+++ BRCA/ui.R
@@ -14,7 +14,7 @@ shinyUI(fluidPage(
 			
 		),
 		mainPanel(
-			HTML("Thousands of mutations in the BRCA1 and BRCA2 genes have been documented. 23andMe reports data for three mutations (two in BRCA1 and one in BRCA2) that account for 80-90% of inherited breast and ovarian cancer among women with Ashkenazi Jewish ancestry. Other mutations in these two genes are not included in the 23andme report. Many can only be detected by sequencing, such as from myriad genetics. A few extra of interest can be reached with imputation analysis as follows.<br><br>"),
+			HTML("Thousands of mutations in the BRCA1 and BRCA2 genes have been documented. 23andMe reports data for three mutations that account much of inherited breast cancer, but other possible mutations in these two genes are not included in the 23andme report. Many can only be detected by sequencing, such as from myriad genetics. However, a few hundred extra possible mutations of interest can be reached with imputation analysis. The following lists your genotype for the directly measured three 23andme-SNPs as well as all other SNPs in the two genes that are either missense or nonsense. For interpretation we recommend reading more about <a href='http://genetics.bwh.harvard.edu/pph2/'>polyphen</a> and <a href='http://sift.jcvi.org/'>sift</a>-scores.<br><br>"),
 			dataTableOutput("table1")
 			# plotOutput("plot1")
 			
