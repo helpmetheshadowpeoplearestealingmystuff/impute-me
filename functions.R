1ab2891fa9c3e745905c9d9e37bf1176b3794472
lassefolkersen <lassefolkersen@gmail.com>
2015-10-15 09:39:17 +0200

    s

--- functions.R
+++ functions.R
@@ -614,7 +614,11 @@ get_GRS<-function(genotypes, betas){
 	
 	if(class(genotypes)!="data.frame")stop(paste("genotypes must be data.frame, not",class(genotypes)))
 	if(!"genotype"%in%colnames(genotypes))stop(paste("genotypes must have a column genotypes"))
-	if(!all(unique(sub("[0-9].+$","",rownames(genotypes)))%in%c("i","rs")))stop("genotypes must have rownames starting with rs")
+	if(!all(unique(sub("[0-9].+$","",rownames(genotypes)))%in%c("i","rs"))){
+		
+		stop(paste("genotypes must have rownames starting with rs. You had these:",paste(unique(sub("[0-9].+$","",rownames(genotypes))),collapse=", ")))
+		
+	}
 	
 	if(class(betas)!="data.frame")stop(paste("genotypes must be data.frame, not",class(betas)))
 	necessary_columns<-c("effect_allele","non_effect_allele","Beta")
