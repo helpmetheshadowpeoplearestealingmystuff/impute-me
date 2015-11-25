10190d9affa26d70bd18570c77581ddebc0d7bda
lassefolkersen <lassefolkersen@gmail.com>
2015-11-25 10:13:08 +0100

    s

--- start/ui.R
+++ start/ui.R
@@ -14,6 +14,7 @@ shinyUI(fluidPage(
 			a("RareGene", href="http://www.impute.me/placeholder.html"),br(),
 			a("MyOffspring", href="http://www.impute.me/placeholder.html"),br(),
 			a("BRCA", href="http://www.impute.me/BRCA"),br(),
+			a("Statins", href="http://www.impute.me/statins"),br(),
 			a("Contact", href="http://www.impute.me/contact"),br(),
 		
 			
@@ -41,7 +42,9 @@ shinyUI(fluidPage(
 			<h2><a href='http://www.impute.me/hairColour'>GuessMyHairColour</a> </h2>A module that attempts to guess your likely hair-colour using both black-brown-blonde scales and red-or-not scales in combination. Currently very much in beta-testing. If you have red-hair please write me and help with the tuning of the parameters.
 			<h2><a href='http://www.impute.me/placeholder.html'>Rare gene discovery</a></h2>This module searches the genome of healthy adults for genes that are completely broken. The idea here is to use this as base for discovery projects on the thousands of genes in the genome that we know very little about. The existence of a broken or seriously altered version of an unknown gene in a healthy adult will be highly beneficial to the scientific community. Only use this module if you have an interest in contributing to science. Not implemented yet.
 			<h2><a href='http://www.impute.me/placeholder.html'>Offspring trait prediction</a></h2>The height and hair module, just for use with children that are not even conceived yet. Not implemented yet. (Warning for larger confidence intervals).
-			<h2><a href='http://www.impute.me/BRCA'>Enhanced BRCA2 investigation</a></h2>While this information is available for people who have access to the 23andme health area, the availability of imputed data improves the coverage somewhat and allows to catch some rare, maybe deleterious, mutations.")
+			<h2><a href='http://www.impute.me/BRCA'>Enhanced BRCA2 investigation</a></h2>While this information is available for people who have access to the 23andme health area, the availability of imputed data improves the coverage somewhat and allows to catch some rare, maybe deleterious, mutations.
+					 <h2><a href='http://www.impute.me/statins'>Statin induced cardiomyopathy</a></h2>A comment on recent specific-gene testing sold in the US. This module provides the same info as a $99 dollar specific test does."
+					 )
 
 		)
 	)
