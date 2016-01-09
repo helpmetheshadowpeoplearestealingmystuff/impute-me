fdaeaac3066b80d08a5edb48f999d1344802a480
root <root@ip-172-31-23-204.us-west-2.compute.internal>
2016-01-09 09:47:40 +0000

    Changes to ui.R for testing SD's novel interface

--- start/ui.R
+++ start/ui.R
@@ -1,40 +1,27 @@
 
 
-tags$head(include('/layout/head.html'))
+#tags$head(includeHTML('../layout/head.html'))
 
 shinyUI(fluidPage(
-	include('/layout/navigation.html'),
+	includeHTML('../layout/navigation.html'),
 	titlePanel("Impute.me"),
 	sidebarLayout(
 		mainPanel(
-			HTML("<h1>Welcome to impute.me</h1>
-				<br>
-			<br>
+			HTML("<h1>Welcome to impute.me</h1><br><br>
 
 
-			This is the starting page for a cutting edge genetics analysis tools.<br><br>
-
-			It can contribute better and more useful genetic analysis - if you already have genome-wide data from a direct-to-consumer service.<br><br>
-
-		Because of expenses related to the building of the web-site it is currently only for paying backers of our <a href='http://kck.st/1VlrTlf'>kickstarter campaign</a> campaign. The project however is fundamentally non-profit and aim to driven only by donations<br><br>
-
-		We take your anonymity much more seriously than any other online genetics-service: Your genome is only kept for two weeks for download and analysis, after which it is deleted from our servers. Also, we only want as little information about you as absolute required: an email to send the results to is the only requirement.<br><br>
-
-	The code for the server is open-source and freely available at <a href='https://github.com/lassefolkersen/gene-surfer'>github</a>. These are the current modules:<br>
-			<h2><a href='http://www.impute.me/imputeme'>ImputeMe</a> </h2>The core of the analysis engine. This advanced algorithm will take the 0.7M genotypes that customers of direct-to-consumer genetic testing companies usually receive. It will then analyze them for about 24 hours, using ethnicity information from the 1000 genomes project. Afterwards it will return an expanded version of 4M known imputed genotypes - 'guessed', but at a very high confidence level.
-			<h2><a href='http://www.impute.me/guessMyHeight'>GuessMyHeight</a> </h2>A module that uses the 697 recently discovered main-drivers of variability in human height, and make a prediction of your height. In the majority of cases this should be accurate to within +/-5 cms, but do keep in mind that environment also plays a large role in most traits. For children this should be taken as expected final height.
-			<h2><a href='http://www.impute.me/hairColour'>GuessMyHairColour</a> </h2>A module that attempts to guess your likely hair-colour using both black-brown-blonde scales and red-or-not scales in combination. Currently very much in beta-testing. If you have red-hair please write me and help with the tuning of the parameters.
-			<h2><a href='http://www.impute.me/nonsenser'>Rare gene discovery</a></h2>This module searches the genome of healthy adults for genes that are completely broken. The idea here is to use this as base for discovery projects on the thousands of genes in the genome that we know very little about. The existence of a broken or seriously altered version of an unknown gene in a healthy adult will be highly beneficial to the scientific community. Only use this module if you have an interest in contributing to science. Just implemented! (2015-12-16)
-			<h2><a href='http://www.impute.me/placeholder.html'>Offspring trait prediction</a></h2>The height and hair module, just for use with children that are not even conceived yet. Not implemented yet. (Warning for larger confidence intervals).
-			<h2><a href='http://www.impute.me/BRCA'>Enhanced BRCA2 investigation</a></h2>While this information is available for people who have access to the 23andme health area, the availability of imputed data improves the coverage somewhat and allows to catch some rare, maybe deleterious, mutations.
-					 <h2><a href='http://www.impute.me/statins'>Statin induced cardiomyopathy</a></h2>A comment on recent specific-gene testing sold in the US. This module provides the same info as a $99 dollar specific test does. Also try the <a href='http://www.impute.me/athletics'>athletics module </a> which similarly provides the same data that an otherwise <a href='https://www.athgene.com/'>expensive specialized test</a> would do."
-					 )
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat lacinia nibh, vel eleifend sapien pulvinar non. Morbi arcu quam, molestie id vestibulum sodales, consequat gravida leo. Etiam tempor dictum neque in tristique. Nulla facilisi. Donec malesuada, lorem vitae lobortis tristique, ex nulla ullamcorper metus, feugiat ullamcorper purus neque a quam. Sed lobortis elit pulvinar nulla blandit, non bibendum lorem aliquet. Aliquam ut luctus nibh.
+<br><br>
+Nulla ut rutrum metus. Vestibulum scelerisque neque leo, nec pharetra ipsum viverra ut. Suspendisse non sapien vehicula, placerat ligula ac, dignissim quam. Curabitur non tortor ultricies arcu commodo lobortis. Morbi risus nisi, iaculis id risus quis, lobortis ullamcorper lorem. Phasellus quis odio a neque vestibulum ornare. Sed aliquam, velit nec placerat vehicula, sem risus interdum lacus, non laoreet ligula nisl quis magna. Donec vel tempor tortor. Morbi tincidunt vulputate augue ac vehicula. Fusce suscipit nunc ut dictum ultricies. In pharetra nibh non tristique porta. Vivamus ut est feugiat, varius sem id, fermentum mi. In hac habitasse platea dictumst.
+	<br><br>
+In ut iaculis ipsum. Maecenas posuere, magna quis auctor dapibus, odio ex molestie augue, at varius nunc ipsum id mi. Praesent tellus nisl, pellentesque nec est a, ultricies porta mauris. In ultrices dui a ligula elementum, eu maximus arcu tristique. Morbi eget erat quis leo commodo interdum eu eu magna. Nam molestie augue sapien, at volutpat metus dictum a. In hac habitasse platea dictumst. In hac habitasse platea dictumst. Maecenas semper non ipsum a congue. Praesent fringilla eleifend rhoncus. Sed volutpat vitae elit eu egestas. Nullam sed venenatis mi. Cras semper eu purus vitae mattis.
+"
 
 		)
 	),
 
-	include('/layout/footer.html'),
-))
+	includeHTML('../layout/footer.html')
+)))
 
 
 
@@ -48,14 +35,3 @@ shinyUI(fluidPage(
 
 
 
-
-# p("Donations to keep server running are gladly accepted"),
-# img(src = "http://i.imgur.com/C823ybI.png", height = 100, width = 100),
-# 			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-# 				<input type="hidden" name="cmd" value="_s-xclick">
-# 				<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHVwYJKoZIhvcNAQcEoIIHSDCCB0QCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAuCPKkSPJuKTXKjXoDXHn2b707QWYDK6JtYIRj05gzZqRpaiful/1lJg7RBG2HocWw5RxR6YoU1tiNm2g5WR8GuvRJ/LVJjlgj3QyRXJKjjuK/SYViP5bytuuHbSlg8jg9R4MjvrtomuqALJ6Kk7H26BUq2FC0WCaxNcvNARdCtDELMAkGBSsOAwIaBQAwgdQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIi/93Wve6baeAgbAuf8X8eGGltaXdo/SVmMQfPRp7O3ZJKjKIBKPRS9DgTJUBdrHecKMUynso2rW2jcA37dk5oOsR+UHbEBuYGm4gYDw6xf3XIMnKgSYG6ng8cY8l6cITkcim0avyZOKWjgGa4q1kd71dRMyPlQ2Ge3e+5QG3WzKPMcwxzn0aTvD6y9DaMmQjRuPVf5/QhxL408yN6UlAxn9S4FwBsAIFawMMxizefz4iJk2Oe+gUPTeZH6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE1MDgyNDExNTUyNVowIwYJKoZIhvcNAQkEMRYEFIstgIuH+50dMxucgigQWG8b9K7yMA0GCSqGSIb3DQEBAQUABIGAlrStYbnByKYH42GqXllMr2j7QJxos2GqBavyHYXEIH5UzbP16E0JJCy4J5DYZ2KUkVL3SrfCFVYE9wInmneNTeZnpozd41uNx0dI7YVH5as8KyGcVmwHrsNjAVvzsT/5SY5j1x5y8+nJaOBdPvQjjtjLMCaS0llE+/tZjIWijl4=-----END PKCS7-----
-# ">
-# 				<input type="image" src="https://www.paypalobjects.com/da_DK/DK/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – den sikre og nemme måde at betale på nettet.">
-# 				<img alt="" border="0" src="https://www.paypalobjects.com/da_DK/i/scr/pixel.gif" width="1" height="1">
-# 				</form>
-# 				
