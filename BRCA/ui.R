99b2ca698b2c9fdb5374ac478ec33ce37266cd41
root <root@ip-172-31-23-204.us-west-2.compute.internal>
2016-01-26 13:23:50 +0000

    BRCA ui.R fixed (remove sidebarpanel)

--- BRCA/ui.R
+++ BRCA/ui.R
@@ -2,21 +2,19 @@
 
 
 shinyUI(fluidPage(
-	tags$head(includeHTML('../layout/head.html')),
+	includeHTML('../layout/head.html'),
 	includeHTML('../layout/navigation.html'),
 	titlePanel("Expanded BRCA info"),
 	sidebarLayout(
-		sidebarPanel(
-			
-			textInput(inputId="uniqueID", label = "Unique ID", value = "id_XXXXXXXXX"),
-			actionButton("goButton","Run analysis"),
-			width=4
-			
-			
-			
-		),
+	#	sidebarPanel(
+	#		textInput(inputId="uniqueID", label = "Unique ID", value = "id_XXXXXXXXX"),
+	#		actionButton("goButton","Run analysis"),
+	#		width=4
+	#	),
 		mainPanel(
-			HTML("Thousands of mutations in the BRCA1 and BRCA2 genes have been documented. 23andMe reports data for three mutations that account much of inherited breast cancer, but other possible mutations in these two genes are not included in the 23andme report. Many can only be detected by sequencing, such as from myriad genetics. However, a few hundred extra possible mutations of interest can be reached with imputation analysis. The following lists your genotype for the directly measured three 23andme-SNPs as well as all other SNPs in the two genes that are either missense or nonsense. For interpretation we recommend reading more about <a href='http://genetics.bwh.harvard.edu/pph2/'>polyphen</a> and <a href='http://sift.jcvi.org/'>sift</a>-scores.<br><br>"),
+			textInput(inputId="uniqueID", label = "", value = "id_XXXXXXXXX"),
+			actionButton("goButton","Run analysis"),
+			HTML("<br><br>Thousands of mutations in the BRCA1 and BRCA2 genes have been documented. 23andMe reports data for three mutations that account much of inherited breast cancer, but other possible mutations in these two genes are not included in the 23andme report. Many can only be detected by sequencing, such as from myriad genetics. However, a few hundred extra possible mutations of interest can be reached with imputation analysis. The following lists your genotype for the directly measured three 23andme-SNPs as well as all other SNPs in the two genes that are either missense or nonsense. For interpretation we recommend reading more about <a href='http://genetics.bwh.harvard.edu/pph2/'>polyphen</a> and <a href='http://sift.jcvi.org/'>sift</a>-scores.<br><br>"),
 			dataTableOutput("table1")
 			# plotOutput("plot1")
 			
