08d48b1ae0bffae35bb2665ad9f4791ee430d820
lassefolkersen <lassefolkersen@gmail.com>
2016-02-05 10:23:11 +0100

    Updates start punch-line (for early live-release) and contact page

--- contact/ui.R
+++ contact/ui.R
@@ -6,7 +6,9 @@ shinyUI(bootstrapPage(
 	navigation(),
 	titlePanel("Contact"),
 	beginPage(),
-	HTML(	"This page is created and operated by <a href='http://www.cbs.dtu.dk/staff/show-staff.php?id=1202'>Lasse Folkersen</a>.<br><br>I work as a genetics researcher at the Technical University of Denmark. <br><br>Feel free to write me an <a href='http://www.google.com/recaptcha/mailhide/d?k=01pdzWyCfeU-_1PRAPdKlJfg==&amp;c=3eyQPG-VqkHu6ECGRBSHdRraKCXOUsVtLpuyWWt-dpY=' onclick='window.open('http://www.google.com/recaptcha/mailhide/d?k\07501pdzWyCfeU-_1PRAPdKlJfg\75\75\46c\0753eyQPG-VqkHu6ECGRBSHdRraKCXOUsVtLpuyWWt-dpY\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;' title='Reveal this e-mail address'>email</a>."),
+	HTML(	"Genetics lead: <a href='http://www.cbs.dtu.dk/staff/show-staff.php?id=1202'>Lasse Folkersen</a>. <a href='http://www.google.com/recaptcha/mailhide/d?k=01pdzWyCfeU-_1PRAPdKlJfg==&amp;c=3eyQPG-VqkHu6ECGRBSHdRraKCXOUsVtLpuyWWt-dpY=' onclick='window.open('http://www.google.com/recaptcha/mailhide/d?k\07501pdzWyCfeU-_1PRAPdKlJfg\75\75\46c\0753eyQPG-VqkHu6ECGRBSHdRraKCXOUsVtLpuyWWt-dpY\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;' title='Reveal this e-mail address'>email</a>.<br>
+				Design and interface lead: Stefan Delport
+				"),
 # 	endPage(),
 	footer()
 ))
