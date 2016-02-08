f712ce2e514e66e984869289a137be71b5c2ea21
lassefolkersen <lassefolkersen@gmail.com>
2016-02-08 15:16:09 +0100

    starting with pharmacogenetics

--- statins/server.R
+++ statins/server.R
@@ -9,7 +9,43 @@ source("/srv/shiny-server/gene-surfer/functions.R")
 # Define server logic for random distribution application
 shinyServer(function(input, output) {
 	
+	
 	output$table1 <- renderDataTable({ 
+		# Take a dependency on input$goButton
+		
+		if(input$goButton == 0){
+			return(NULL)
+		}else if(input$goButton > 0) {
+			print(paste("Ok",input$goButton))
+		}
+		
+		uniqueID<-isolate(input$uniqueID)
+		if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
+		if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
+		pDataFile<-paste("/home/ubuntu/data/",uniqueID,"/pData.txt",sep="")
+		
+		if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
+			Sys.sleep(3) #wait a little to prevent raw-force fishing	
+			stop("Did not find a user with this id")
+		}
+
+		
+		table_file <-"/srv/shiny-server/gene-surfer/statins/SNPs_to_analyze.txt"
+		table<-read.table(SLCO1B1_table_file,sep="\t",header=T,stringsAsFactors=F)
+		rownames(table)<-table[,"SNP"]
+		genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
+		
+		table[,"Your genotype"]<-genotypes[rownames(table),]
+		table<-table[,c("SNP","Your genotype","effect_allele")]
+		colnames(table)<-c("SNP","Your genotype","Risk allele")
+		table<-table["rs2395029",,drop=FALSE]
+		return(table)
+
+	})
+	
+	
+	
+	output$table2 <- renderDataTable({ 
 		# Take a dependency on input$goButton
 		
 		if(input$goButton == 0){
@@ -45,7 +81,7 @@ shinyServer(function(input, output) {
 
 		SLCO1B1_table<-SLCO1B1_table[,c("SNP","Your genotype","effect_allele")]
 		colnames(SLCO1B1_table)<-c("SNP","Your genotype","Risk allele")
-		
+		SLCO1B1_table<-SLCO1B1_table["rs4363657",,drop=FALSE]
 		return(SLCO1B1_table)
 		
 		
--- statins/ui.R
+++ statins/ui.R
@@ -8,7 +8,7 @@ shinyUI(bootstrapPage(
 	titlePanel("Statin induced cardiomyopathy"),
 	beginPage(),
 	beginPanel('1/3'),
-	HTML("Some genetic tests can be bought, that are directed against just one specific gene. An example of this include the rs4363657/SLCO1B1 test sold by <u><a href='http://www.bostonheartdiagnostics.com/science_portfolio_statin.php'>Boston Heart Diagnostics</a></u> for 99$. However, when buying genome-wide kits such as those sold by ancestry.com or 23andme you can obtain the very same information from your raw data, at no extra cost. In this module that analysis is provided.<br><br>To run analysis input your user-id, or use the test-value of id_57n662948:<br><br>"),
+	HTML("Some drug response genetic tests can be bought, that are directed against just one specific gene. However, when buying genome-wide kits such as those sold by ancestry.com or 23andme you can obtain the very same information from your raw data, at no extra cost. In this module that analysis is provided.<br><br>To run analysis input your user-id, or use the test-value of id_57n662948:<br><br>"),
 	
 	textInput(inputId="uniqueID", label = "Unique ID", value = "id_XXXXXXXXX"),
 	actionButton("goButton","Run analysis"),
@@ -16,10 +16,13 @@ shinyUI(bootstrapPage(
 	endPanel(),
 	beginPanel('2/3'),
 	
-	h2("Statin response"),
-	HTML("This is based on the <u><a href='http://www.bostonheartdiagnostics.com/science_portfolio_statin.php'>Boston Heart Diagnostics</a></u> test. If you carry one or two 'C' alleles this is equal to a positive test.<br><br>
-	Of note, however, it is a silly idea that you should worry about statins based on this information. The positive prediction rate is 5%, meaning that in 95% of the cases you will not have a problem even though your genotype contains a C. That hasn't stopped 250k US doctors from ordering the test. Here you can have it automatically:<br><br>"),
+	h2("HIV treatment response"),
+	HTML("This is a fairly rare SNP, for which T-alleles are associated with adverse response to the <u><a href='http://www.ncbi.nlm.nih.gov/pubmed/18684101'>HIV-drug abacavir</a></u>. It is sometimes tested for before initiating abacavir treatment. Note however, that the studies supporting this are specifically only valid for caucasian ethnicities. Several other ethnicities are much more likely to have a T-allele, without problems.")
 	dataTableOutput("table1"),
+	
+	h2("Statin response"),
+	HTML("This is based on the <u><a href='http://www.bostonheartdiagnostics.com/science_portfolio_statin.php'>Boston Heart Diagnostics</a></u> test, which sells for 99$. The interpretation is that if you carry one or two 'C' alleles it is equal to a positive test.	Of note, however, it is a silly idea that you should worry about statins based on this information. The positive prediction rate is 5%, meaning that in 95% of the cases you will not have a problem even though your genotype contains a C. That hasn't stopped 250k US doctors from ordering the test. Here you can have it automatically:<br><br>"),
+	dataTableOutput("table2"),
 	
 	endPanel(),
 	
