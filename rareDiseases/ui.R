e17c1a865e54bab41e5057d7dee210364ce2e5f7
lassefolkersen <lassefolkersen@gmail.com>
2016-02-10 14:28:04 +0100

    opening rareDiseases up for navigation access

--- rareDiseases/ui.R
+++ rareDiseases/ui.R
@@ -9,7 +9,7 @@ shinyUI(bootstrapPage(
 	titlePanel("Rare diseases"),
 	beginPage(),
 	beginPanel('1/3'),
-	HTML("Users with 23andme data had a special 'inherited conditions' analysis tool. However, this was taken off-line leaving only ancestry information availble. In this module the original 'inhereted conditions' are re-analysed.<br>To run analysis input your user-id, or use the test-value of id_57n662948:<br><br>"),
+	HTML("Users with 23andme data had a special 'inherited conditions' analysis tool. However, this was taken off-line leaving only ancestry information availble. In this module the original 'inherited conditions' are re-analysed.<br><br>To run analysis input your user-id, or use the test-value of id_57n662948:<br><br>"),
 	textInput(inputId="uniqueID", label = "", value = "id_XXXXXXXXX"),
 	actionButton("goButton","Run analysis"),
 	endPanel(),
@@ -17,7 +17,7 @@ shinyUI(bootstrapPage(
 	
 	
 	endPanel(),
-	HTML("This table shows the variants which are known to confer a range of severe inherited conditions. They are all fairly rare conditions. Slightly more common, however, is the case of being a carrier of these conditions. Being a carrier means that a person have one copy of a disease-causing allele, but will not be affected because the condition only manifests if both copies are of the disease-causing type.<br><br><br>"),
+	HTML("The table shows the variants which are known to confer a range of severe inherited conditions. They are all fairly rare conditions. Slightly more common, however, is the case of being a carrier of these conditions. Being a carrier means that a person have one copy of a disease-causing allele, but will not be affected because the condition only manifests if both copies are of the disease-causing type.<br><br><br>"),
 	htmlOutput("text_advice1"),
 	HTML("<br><br><br>"),
 	dataTableOutput("table1"),
