a9688c243e93b0266941d8a28385c461a98e47c5
lassefolkersen <lassefolkersen@gmail.com>
2016-02-10 12:20:45 +0100

    typo

--- rareDiseases/ui.R
+++ rareDiseases/ui.R
@@ -17,9 +17,9 @@ shinyUI(bootstrapPage(
 	
 	
 	endPanel(),
-	HTML("This table shows the variants which are known to confer a range of severe inherited conditions. They are all fairly rare conditions. Slightly more common, however, is the case of being a carrier of these conditions. Being a carrier means that a person have one copy of a disease-causing allele, but will not be affected because the condition only manifests if both copies are of te disease-causing type.<br>"),
-	
+	HTML("This table shows the variants which are known to confer a range of severe inherited conditions. They are all fairly rare conditions. Slightly more common, however, is the case of being a carrier of these conditions. Being a carrier means that a person have one copy of a disease-causing allele, but will not be affected because the condition only manifests if both copies are of the disease-causing type.<br><br><br>"),
 	htmlOutput("text_advice1"),
+	HTML("<br><br><br>"),
 	dataTableOutput("table1"),
 	# dataTableOutput("table1"),
 	
