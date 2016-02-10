6899228b6eddb32db9e1a7c8656857d91fcd55e4
lassefolkersen <lassefolkersen@gmail.com>
2016-02-10 10:58:27 +0100

    starting the rare conditions page

---
+++ rareDiseases/ui.R
@@ -0,0 +1,36 @@
+
+source("../uifunctions.R")
+initialize('ath',TRUE)
+
+
+shinyUI(bootstrapPage(
+	head(),
+	navigation(),
+	titlePanel("Rare diseases"),
+	beginPage(),
+	beginPanel('1/3'),
+	HTML("Users with 23andme data had a special 'inherited conditions' analysis tool. However, this was taken off-line leaving only ancestry information availble. In this module the original 'inhereted conditions' are re-analysed:"),
+	textInput(inputId="uniqueID", label = "Unique ID", value = "id_XXXXXXXXX"),
+	actionButton("goButton","Run analysis"),
+	endPanel(),
+	beginPanel('2/3'),
+	
+	
+	endPanel(),
+	HTML("This table shows the variants which are known to confer a range of severe inhereted conditions. They are all fairly rare conditions. Slightly more common, however, is the case of being a carrier of the conditions. Being a carrier means that a person have one copy of a disease-causing allele, but will not be affected because the condition only manifests if both copies are of te disease-causing type."),
+	tableOutput("table1"),
+	# dataTableOutput("table1"),
+	
+	
+	endPage(),
+	footer()
+	
+)	
+)
+
+
+
+
+
+
+
