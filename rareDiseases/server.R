13b2bca03cd9a24482af6d01cc6f327109f9a4dc
lassefolkersen <lassefolkersen@gmail.com>
2016-02-10 11:52:29 +0100

    That worked! Then try to report full table

--- rareDiseases/server.R
+++ rareDiseases/server.R
@@ -28,10 +28,10 @@ shinyServer(function(input, output) {
 		genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
 		
 		table[,"Your genotype"]<-genotypes[rownames(table),]
-		# table<-table[,c("SNP","Your genotype","risk_allele","non_risk_allele","disease_name")]
-		# colnames(table)<-c("SNP","Your genotype","Risk-allele","Non-Risk-allele","Inherited Condition")
-		table<-table[,c("SNP","Your genotype","disease_name")]
-		colnames(table)<-c("SNP","Your genotype","Inherited Condition")
+		table<-table[,c("SNP","Your genotype","risk_allele","non_risk_allele","disease_name")]
+		colnames(table)<-c("SNP","Your genotype","Risk-allele","Non-Risk-allele","Inherited Condition")
+		# table<-table[,c("SNP","Your genotype","disease_name")]
+		# colnames(table)<-c("SNP","Your genotype","Inherited Condition")
 		
 		return(table)
 	})
