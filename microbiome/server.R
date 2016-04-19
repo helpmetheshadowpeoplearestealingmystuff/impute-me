72af83061ff45e3d5fa5d94c8c4e703b218eb33c
Lasse Folkersen <FOLK@lundbeck.com>
2016-04-19 11:46:06 +0200

    first microbiome draft

---
+++ microbiome/server.R
@@ -0,0 +1,40 @@
+library("shiny")
+source("/srv/shiny-server/gene-surfer/functions.R")
+
+
+#Replace 'template' with name of module throughout the script
+
+# Define server logic for a template
+shinyServer(function(input, output) {
+	output$table1 <- renderTable({ 
+		if(input$goButton == 0){
+			return(NULL)
+		}
+		uniqueID<-isolate(input$uniqueID)
+		if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
+		if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
+		if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
+			Sys.sleep(3) #wait a little to prevent raw-force fishing	
+			stop("Did not find a user with this id")
+		}
+
+		
+		
+		
+		table_file <-"../template/SNPs_to_analyze.txt"
+		table<-read.table(table_file,sep="\t",header=T,stringsAsFactors=F)
+		rownames(table)<-table[,"SNP"]
+		#This will return a copy of the SNPs_to_analyze.txt, with the genotypes of this specific person (=uniqueID) as a new column
+		genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
+		table[,"Your genotype"]<-genotypes[rownames(table),]
+		
+		
+		
+		table <- table[,c("SNP","Increasing_allele","Bacteria")]
+		
+		#
+		
+		return(table)
+		
+	},include.rownames = FALSE)
+})
---
+++ microbiome/ui.R
@@ -0,0 +1,36 @@
+
+source("../uifunctions.R")
+initialize('sti',TRUE)
+
+shinyUI(bootstrapPage(
+	head(),
+	navigation(),
+	titlePanel("template"),
+	beginPage(),
+	beginPanel('1/3'),
+	HTML("Some text that will go above the input ID place"),
+	
+	textInput(inputId="uniqueID", label = "Unique ID", value = "id_XXXXXXXXX"),
+	actionButton("goButton","Run analysis"),
+	
+	endPanel(),
+	beginPanel('2/3'),
+	
+	h2("Overview of bacterial abundance of various gut bacteria"),
+	HTML("Throughout the last decade the human gut microbiome have been extensively explored utilising emergent high throughtput techniques. Through these studies the microbiome have been associated with various conditions ranging from metabolic diseases and irritable bowel syndrome to mental disorders like autism spectrum disorder. In general, low diversity of microbial species in the gut is related to various disease states and connections between specific species and dysbiosis are being unraveled within this relative young field of study."),
+	tableOutput("table1"), #getting the table created in server.R
+
+	endPanel(),
+	
+	# Optional alternative for when you want to show a plot
+	# plotOutput("plot1")
+	
+	endPage(),
+	footer()
+))
+
+
+
+
+
+
