1a5644e017882936f60b38c0ccab342fea9249f2
Lasse Folkersen <FOLK@lundbeck.com>
2016-05-20 11:29:28 +0800

    down-toning the athletics section a bit

--- athletics/ui.R
+++ athletics/ui.R
@@ -13,7 +13,7 @@ shinyUI(bootstrapPage(
 	actionButton("goButton","Run analysis"),
 	endPanel(),
 	beginPanel('2/3'),
-	HTML("Some genetic tests can be bought, that claim That you can learn about your pre-dispositions in fitness, nutrition and sleep. While this claim is largely exagerated, it is true that science knows about genetic variation that have impact on human fitness. These tests are sold for approximately 200USD, giving you knowledge of handful of specific fitness related SNPs. Of course the very same information is also available for free if you already have imputed genotyping microarray information as part of this site. 
+	HTML("There exists SNPs that give various pre-dispositions for fitness-levels. While the these power-vs-endurance variants are clearly reproducible throughout many studies, their effect is still rather limited - meaning that one can be a world-class sprinter without necessarily having 'the sprinter gene'. Many tests are sold for these specific SNPs, giving you knowledge of handful of specific fitness related SNPs. Of course the very same information is also available for free if you already have imputed genotyping microarray information. 
 
 Provided in this module is therefore an overview of your fitness related SNPs. For further information on each, we refer to the litterature detailing each of the supporting information pieces.<br><br>"),
 	
