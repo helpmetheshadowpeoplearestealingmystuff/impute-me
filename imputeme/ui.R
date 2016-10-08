c5dad5b94bc21ca7f95c1d700e2c71549722514c
Lasse Folkersen <FOLK@lundbeck.com>
2016-10-08 06:08:13 +0200

    video link

--- imputeme/ui.R
+++ imputeme/ui.R
@@ -17,7 +17,7 @@ shinyUI(bootstrapPage(
 	
 	endPanel(),
 	beginPanel('2/3'),
-	HTML("Imputation is a cutting edge computational technology that takes genetic measurements such as those provided by 23andme or ancestry.com and proceeds to impute - or 'guess' millions of additional genetic variations that were not measured in the original data. This is done based on overall knowledge of human ethnicity and ancestry and is further explained in the video of this <u><a href='https://www.kickstarter.com/projects/1563061294/impute-my-genome'>kickstart project</a></u>. The first step in using this site is to upload your genetic data, and then wait for the analysis to run a few days. After that you'll receive an email with an ID-login that you can use to explore the remaining analysis pages.<br><br>
+	HTML("Imputation is a cutting edge computational technology that takes genetic measurements such as those provided by 23andme or ancestry.com and proceeds to impute - or 'guess' millions of additional genetic variations that were not measured in the original data. This is done based on overall knowledge of human ethnicity and ancestry and is further explained in the video of this <u><a href='https://www.kickstarter.com/projects/1563061294/impute-my-genome/description'>kickstart project</a></u>. The first step in using this site is to upload your genetic data, and then wait for the analysis to run a few days. After that you'll receive an email with an ID-login that you can use to explore the remaining analysis pages.<br><br>
 			 This service is currently free, but donations are very welcome since the server requirements for analysis are quite high. Also the service is completely anonymous <br><br><br>"),
 	
 	textOutput("text1"),
