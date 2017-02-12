3124694beae3b652efa01acc13f2050f73da8864
Lasse Folkersen <FOLK@lundbeck.com>
2017-02-12 23:07:48 +0100

    finishing touches for today

--- imputeme/deletion_cron_job.R
+++ imputeme/deletion_cron_job.R
@@ -4,13 +4,13 @@
 
 source("/srv/shiny-server/gene-surfer/functions.R")
 
-folders<-list.files("/home/ubuntu/data")
+uniqueIDs<-list.files("/home/ubuntu/data")
 
 #in days days
 keeping_time<-14
 
-for(folder in folders){
-	pData<-try(read.table(paste("/home/ubuntu/data/",folder,"/pData.txt",sep=""),sep="\t",header=T,stringsAsFactors=F))
+for(uniqueID in uniqueIDs){
+	pData<-try(read.table(paste("/home/ubuntu/data/",uniqueID,"/pData.txt",sep=""),sep="\t",header=T,stringsAsFactors=F))
 	if(class(pData)=="try-error")next
 	if(!all(c("protect_from_deletion","first_timeStamp")%in%colnames(pData)))next
 	if(!pData[1,"protect_from_deletion"]){
@@ -18,14 +18,24 @@ for(folder in folders){
 		start<-strptime(pData[1,"first_timeStamp"],"%Y-%m-%d-%H-%M")
 		timedif<-difftime(Sys.time(),start, units="days")
 		if(timedif > keeping_time){
-			print(paste("Deleting",folder,"because it is",timedif,"days old"))	
+			print(paste("Deleting",uniqueID,"because it is",timedif,"days old"))	
 			
 			if("data" %in% routinely_delete_this){
-				unlink(paste("/home/ubuntu/data/",folder,sep=""),recursive=T)
+				f1<-paste("/srv/shiny-server/gene-surfer/www/",uniqueID,".23andme.zip",sep="")
+				f2<-paste("/srv/shiny-server/gene-surfer/www/",uniqueID,".gen.zip",sep="")
+				f3<-paste("/home/ubuntu/data/",uniqueID,"/",uniqueID,".gen.zip",sep="")
+				f4<-paste("/home/ubuntu/data/",uniqueID,"/",uniqueID,".23andme.zip",sep="")
+				f5<-paste("/home/ubuntu/data/",uniqueID,"/",uniqueID,".input_data.zip",sep="")
+				if(file.exists(f1))unlink(f1)
+				if(file.exists(f2))unlink(f2)
+				if(file.exists(f3))unlink(f3)
+				if(file.exists(f4))unlink(f4)
+				if(file.exists(f5))unlink(f5)
+				
 			}
 			if("link" %in% routinely_delete_this){
-				f1<-paste("/srv/shiny-server/gene-surfer/www/",folder,".gen.zip",sep="")
-				f2<-paste("/srv/shiny-server/gene-surfer/www/",folder,".gen.zip",sep="")
+				f1<-paste("/srv/shiny-server/gene-surfer/www/",uniqueID,".23andme.zip",sep="")
+				f2<-paste("/srv/shiny-server/gene-surfer/www/",uniqueID,".gen.zip",sep="")
 				if(file.exists(f1))unlink(f1)
 				if(file.exists(f2))unlink(f2)
 			}
