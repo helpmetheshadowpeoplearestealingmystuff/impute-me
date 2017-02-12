801a069d9a9f8b7a6ea07f8895ecbe680062ac0f
Lasse Folkersen <FOLK@lundbeck.com>
2017-02-12 19:17:18 +0100

    debugging VII

--- AllDiseases/server.R
+++ AllDiseases/server.R
@@ -40,49 +40,50 @@ shinyServer(function(input, output) {
 	
 	
 	output$plot_1 <- renderPlot({ 
+		
+		trait_pmid<-isolate(input$trait)
+		uniqueID<-isolate(input$uniqueID)
+		
+		
+		# Take a dependency on input$goButton
 		if(input$goButton > 0) {
-			
-			trait_pmid<-isolate(input$trait)
 			uniqueID<-isolate(input$uniqueID)
+			if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
+			if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
+			# pDataFile<-paste(dataFolder,uniqueID,"/pData.txt",sep="")
+			if(!file.exists(paste(dataFolder,uniqueID,sep=""))){
+				Sys.sleep(3) #wait a little to prevent raw-force fishing	
+				stop("Did not find a user with this id")
+			}
 			
 			load(snps_file)
 			
-			# Take a dependency on input$goButton
-			if(input$goButton > 0) {
-				uniqueID<-isolate(input$uniqueID)
-				if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
-				if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
-				# pDataFile<-paste(dataFolder,uniqueID,"/pData.txt",sep="")
-				if(!file.exists(paste(dataFolder,uniqueID,sep=""))){
-					Sys.sleep(3) #wait a little to prevent raw-force fishing	
-					stop("Did not find a user with this id")
-				}
-				
-				trait<-strsplit(trait_pmid," // ")[[1]][1]
-				pmid<-strsplit(trait_pmid," // ")[[1]][2]
-				
-				if(!pmid%in%data[,"PUBMEDID"])stop(paste("PMID",pmid,"was not found in system"))
-				if(!trait%in%data[,"DISEASE.TRAIT"])stop(paste("trait",trait,"was not found"))
-				
-				
-				SNPs_to_analyze<-data[data[,"PUBMEDID"]%in%pmid & data[,"DISEASE.TRAIT"]%in%trait ,]
-				
-				
-				if(any(duplicated(SNPs_to_analyze[,"SNP"])))stop(paste("Some of the SNPs requested for PMID",pmid,"were duplicated"))
-				rownames(SNPs_to_analyze)<-SNPs_to_analyze[,"SNP"]
-				
-				genotypes<-get_genotypes(uniqueID=uniqueID,request=SNPs_to_analyze, namingLabel="cached.all_gwas")
-				GRS_beta <-get_GRS_2(genotypes=genotypes,betas=SNPs_to_analyze)
-			}
+			
+			trait<-strsplit(trait_pmid," // ")[[1]][1]
+			pmid<-strsplit(trait_pmid," // ")[[1]][2]
+			
+			if(!pmid%in%data[,"PUBMEDID"])stop(paste("PMID",pmid,"was not found in system"))
+			if(!trait%in%data[,"DISEASE.TRAIT"])stop(paste("trait",trait,"was not found"))
+			
+			
+			SNPs_to_analyze<-data[data[,"PUBMEDID"]%in%pmid & data[,"DISEASE.TRAIT"]%in%trait ,]
+			
+			
+			if(any(duplicated(SNPs_to_analyze[,"SNP"])))stop(paste("Some of the SNPs requested for PMID",pmid,"were duplicated"))
+			rownames(SNPs_to_analyze)<-SNPs_to_analyze[,"SNP"]
+			
+			genotypes<-get_genotypes(uniqueID=uniqueID,request=SNPs_to_analyze, namingLabel="cached.all_gwas")
+			GRS_beta_all <-get_GRS_2(genotypes=genotypes,betas=SNPs_to_analyze)
+			GRS_beta <- mean(GRS_beta_all,na.rm=T)
+			
 			control_mean<-0
 			control_sd<-1
-			
 			xlim<-c(control_mean - control_sd*3, control_mean + control_sd*3)
 			
 			x<-seq(xlim[1],xlim[2],length.out=100)
 			y_control<-dnorm(x,mean=control_mean,sd=control_sd)
 			plot(x,y_control,type="l",col="blue",ylab="Number of people with this score",xlab="Genetic risk score",yaxt="n",lwd=2)
-
+			
 			
 			#fill the controls
 			if(all(!x<GRS_beta))stop("GRS too low to plot")
@@ -109,9 +110,6 @@ shinyServer(function(input, output) {
 			
 			
 			legend("topleft",legend=c("Population distribution","Your genetic risk score"),lty=c(1,1),lwd=c(2,3),col=c("blue","black"))
-		}else{
-			
-			# legend("topleft",legend=c("People with disease","Healthy people"),lty=c(1,1),lwd=c(2,2),col=c("red","blue"))
 			
 			
 		}
