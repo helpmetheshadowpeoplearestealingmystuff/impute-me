abeeca4884bd0365d10b6e0c7f93f40169f5dcd6
Lasse Folkersen <FOLK@lundbeck.com>
2017-02-13 06:01:49 +0100

    typo III

--- autoimmuneDiseases/server.R
+++ autoimmuneDiseases/server.R
@@ -181,20 +181,22 @@ This plot shows the risk profile for ",dis,". Patients with this disease have ge
 			
 
 			
-			#write the score to the log file
-			log_function<-function(uniqueID,disease,GRS_beta){
-				user_log_file<-paste("/home/ubuntu/data/",uniqueID,"/user_log_file.txt",sep="")
-				m<-c(format(Sys.time(),"%Y-%m-%d-%H-%M-%S"),"autoimmuneDiseases",uniqueID,disease,GRS_beta)
-				m<-paste(m,collapse="\t")
-				if(file.exists(user_log_file)){
-					write(m,file=user_log_file,append=TRUE)
-				}else{
-					write(m,file=user_log_file,append=FALSE)
-				}
-			}
-			try(log_function(uniqueID,disease,GRS_beta))
-			
 		}
+		
+		#write the score to the log file
+		log_function<-function(uniqueID,disease,GRS_beta){
+			user_log_file<-paste("/home/ubuntu/data/",uniqueID,"/user_log_file.txt",sep="")
+			m<-c(format(Sys.time(),"%Y-%m-%d-%H-%M-%S"),"autoimmuneDiseases",uniqueID,disease,GRS_beta)
+			m<-paste(m,collapse="\t")
+			if(file.exists(user_log_file)){
+				write(m,file=user_log_file,append=TRUE)
+			}else{
+				write(m,file=user_log_file,append=FALSE)
+			}
+		}
+		try(log_function(uniqueID,disease,GRS_beta))
+		
+		
 	})
 	
 	
