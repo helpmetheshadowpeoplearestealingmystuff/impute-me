417546d6c4690b740bd7eb9efd580ba7ef42b3cf
Lasse Folkersen <FOLK@lundbeck.com>
2017-02-21 17:32:40 +0100

    Adding the export script functionality

--- functions.R
+++ functions.R
@@ -1281,3 +1281,101 @@ generate_report<-function(uniqueIDs=NULL, filename=NULL){
 	return(relative_webpath)
 }
 
+
+
+
+
+
+
+
+
+
+
+run_export_script<-function(uniqueIDs=NULL,modules=NULL, mode ="remote"){
+  require(jsonlite)
+  #A function that will crawl all module directories and execute the export script if present
+  
+  if(!mode %in% c("remote","local"))stop("mode must be remote or local")
+  if(mode =="remote"){
+    pd<-path_data_remote
+  }else{
+    pd<-path_data
+  }
+  
+  
+  if(is.null(uniqueIDs)){
+    uniqueIDs<-list.files(pd)
+  }else{
+    if(class(uniqueIDs)!="character")stop("UniqueIDs must be of class character")
+    if(!all(file.exists(paste(pd,uniqueIDs,sep=""))))stop("Not all UniqueIDs given were found")
+  }
+  
+  if(is.null(modules)){
+    modules<-list.files(path_imputeme_scripts)
+    
+  }else{
+    if(class(modules)!="character")stop("modules must be of class character")
+    if(!all(file.exists(paste(path_imputeme_scripts,modules,sep=""))))stop("Not all UniqueIDs given were found")
+  }
+  
+  
+  
+  for(uniqueID in uniqueIDs){
+    outputList <- list()
+    outputList[["current_date_stamp"]] <- as.character(format(Sys.time(),"%Y-%m-%d_%H-%M-%S"))
+    #importing standard pData stuff
+    pDataFile<-paste(pd,uniqueID,"/pData.txt",sep="")
+    pData<-try(read.table(pDataFile,header=T,stringsAsFactors=F),silent=T)
+    if(class(pData)=="try-error"){
+      print(paste("uniqueID",uniqueID,"was skipped due to inavailability of pData file"))
+      next
+    }
+    if(nrow(pData)!=1)stop("pData file must have 1 row")
+    
+    #check existence of cached file
+    cachedFile<-paste(pd,uniqueID,"/",uniqueID,".cached.gz",sep="")
+    cachedData<-try(read.table(cachedFile,header=T,stringsAsFactors=F),silent=T)
+    if(class(cachedData)=="try-error"){
+      print(paste("uniqueID",uniqueID,"was skipped due to inavailability of cachedData file"))
+      next
+    }
+    
+    for(imp in c("uniqueID","filename","email","first_timeStamp")){
+      if(!imp %in%colnames(pData))stop(paste("pData lacked this column:",imp))  
+      outputList[[imp]] <-pData[1,imp]
+    }
+    names(outputList)[names(outputList)%in%"filename"] <- "original_filename"
+    names(outputList)[names(outputList)%in%"email"] <- "original_submission_email"
+    
+    
+    for(module in modules){
+      if(!file.info(paste0(path_imputeme_scripts,module))["isdir"])next
+      if("export_script.R" %in% list.files(paste0(path_imputeme_scripts,module))){
+        print(paste("Running",module,"for",uniqueID))
+        if(exists("export_function"))suppressWarnings(rm("export_function"))
+        source(paste(paste0(path_imputeme_scripts,module,"/export_script.R")))
+        if(!exists("export_function"))stop(paste("In module",module,"there was an export_script.R without and export_function"))
+        exp <- export_function(uniqueID, mode=mode)
+        outputList[[module]] <-exp
+        
+      }
+    }
+    
+    JSON<-toJSON(outputList)
+    
+    filename <- paste0(pd,uniqueID,"/",paste(uniqueID,"data.json",sep="_"))
+    
+    
+    f<-file(filename,"w")
+    writeLines(JSON,f)
+    close(f)
+    
+  }
+  
+  m<-paste("The module was successfully run for",length(uniqueIDs),"samples on",length(modules),"modules")
+  
+  
+  return(m)
+}
+
+
