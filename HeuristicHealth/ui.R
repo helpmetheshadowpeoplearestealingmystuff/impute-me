0cc1ddae62152daa5d9b15bd0a9efab1ee991486
Lasse Folkersen <FOLK@lundbeck.com>
2017-02-22 21:50:21 +0100

    v2 video

--- HeuristicHealth/ui.R
+++ HeuristicHealth/ui.R
@@ -23,7 +23,7 @@ shinyUI(bootstrapPage(
 	HTML("'Heuristic', means any approach to problem solving, learning, or discovery that employs a practical method not guaranteed to be optimal or perfect, but sufficient for the immediate goals. Health, that's the context of you. <br><br>
 
 <b>Too much text? Watch a video instead:</b><br>
-<iframe width='560' height='315' src='https://www.youtube.com/embed/AMtBZMWoOPI' frameborder='0' allowfullscreen></iframe><br><br>
+<iframe width='560' height='315' src='https://www.youtube.com/embed/ny8unxAk4JY' frameborder='0' allowfullscreen></iframe><br><br>
 
 <b>Details</b><br>
 So in other words: Except for the few strong-effect cases (the 'long tail' and the 'mendelian' genetics), much of what we can learn from our genomes suffer from being intepreted outside of the current health context: Knowledge of an increased genetic risk of a particular disease-subtype may aid you only if you anyway are being evaluated for a diffuse set of symptoms that include this disease. For example, an increased genetic risk of leukemia may mean very little in a general population, but for patients with systemic joint pain it could be the difference between a wrongful investigation for rheumatoid arthritis or correct investigation for leukemia.<br><br>
