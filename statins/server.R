33a10006ece9705a50117e89fd698ef3e2a03a98
Lasse Folkersen <FOLK@lundbeck.com>
2017-02-28 14:29:29 +0100

    optimizing the way drug response gets data and starting to import Natarajan 2017 data

--- statins/server.R
+++ statins/server.R
@@ -9,23 +9,51 @@ source("/home/ubuntu/srv/impute-me/functions.R")
 # Define server logic for random distribution application
 shinyServer(function(input, output) {
 	
+  
+  
+  get_data <- reactive({
+    
+    #initial UI data gathering and user-check
+    uniqueID<-input$uniqueID
+    if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
+    if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
+    if(!file.exists(paste(dataFolder,uniqueID,sep=""))){
+      Sys.sleep(3) #wait a little to prevent raw-force fishing	
+      stop(paste("Did not find a user with this id",uniqueID))
+    }
+    table_file <-"/home/ubuntu/srv/impute-me/statins/SNPs_to_analyze.txt"
+    table<-read.table(table_file,sep="\t",header=T,stringsAsFactors=F)
+    rownames(table)<-table[,"SNP"]
+    genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
+    
+    table[,"Your genotype"]<-genotypes[rownames(table),]
+    
+    
+    
+    #write the query to the log file
+    log_function<-function(uniqueID){
+      user_log_file<-paste("/home/ubuntu/data/",uniqueID,"/user_log_file.txt",sep="")
+      m<-c(format(Sys.time(),"%Y-%m-%d-%H-%M-%S"),"drug_response",uniqueID)
+      m<-paste(m,collapse="\t")
+      if(file.exists(user_log_file)){
+        write(m,file=user_log_file,append=TRUE)
+      }else{
+        write(m,file=user_log_file,append=FALSE)
+      }
+    }
+    try(log_function(uniqueID))
+    
+    return(table)
+  
+  })
+  
 	output$table1 <- renderTable({ 
 		if(input$goButton == 0){
 			return(NULL)
 		}
-		uniqueID<-isolate(input$uniqueID)
-		if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
-		if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
-		if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
-			Sys.sleep(3) #wait a little to prevent raw-force fishing	
-			stop("Did not find a user with this id")
-		}
-		table_file <-"/home/ubuntu/srv/impute-me/statins/SNPs_to_analyze.txt"
-		table<-read.table(table_file,sep="\t",header=T,stringsAsFactors=F)
-		rownames(table)<-table[,"SNP"]
-		genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
-		
-		table[,"Your genotype"]<-genotypes[rownames(table),]
+	  table<-get_data()
+	  
+	  
 		table<-table[,c("SNP","Your genotype","effect_allele")]
 		colnames(table)<-c("SNP","Your genotype","Risk allele")
 		table<-table["rs2395029",,drop=FALSE]
@@ -39,19 +67,8 @@ shinyServer(function(input, output) {
 		if(input$goButton == 0){
 			return(NULL)
 		}
-		uniqueID<-isolate(input$uniqueID)
-		if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
-		if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
-		if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
-			Sys.sleep(3) #wait a little to prevent raw-force fishing	
-			stop("Did not find a user with this id")
-		}
-		table_file <-"/home/ubuntu/srv/impute-me/statins/SNPs_to_analyze.txt"
-		table<-read.table(table_file,sep="\t",header=T,stringsAsFactors=F)
-		rownames(table)<-table[,"SNP"]
-		genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
-		
-		table[,"Your genotype"]<-genotypes[rownames(table),]
+	  table<-get_data()
+	  
 		table<-table[,c("SNP","Your genotype","effect_allele")]
 		colnames(table)<-c("SNP","Your genotype","Risk allele")
 		table<-table["rs4363657",,drop=FALSE]
@@ -69,19 +86,8 @@ shinyServer(function(input, output) {
 		if(input$goButton == 0){
 			return(NULL)
 		}
-		uniqueID<-isolate(input$uniqueID)
-		if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
-		if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
-		if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
-			Sys.sleep(3) #wait a little to prevent raw-force fishing	
-			stop("Did not find a user with this id")
-		}
-		table_file <-"/home/ubuntu/srv/impute-me/statins/SNPs_to_analyze.txt"
-		table<-read.table(table_file,sep="\t",header=T,stringsAsFactors=F)
-		rownames(table)<-table[,"SNP"]
-		genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
-		
-		table[,"Your genotype"]<-genotypes[rownames(table),]
+	  table<-get_data()
+	  
 		table<-table[,c("SNP","Your genotype","effect_allele")]
 		colnames(table)<-c("SNP","Your genotype","Effect allele")
 		table<-table["rs1799971",,drop=FALSE]
@@ -99,40 +105,15 @@ shinyServer(function(input, output) {
 		if(input$goButton == 0){
 			return(NULL)
 		}
-		uniqueID<-isolate(input$uniqueID)
-		if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
-		if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
-		if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
-			Sys.sleep(3) #wait a little to prevent raw-force fishing	
-			stop("Did not find a user with this id")
-		}
-		table_file <-"/home/ubuntu/srv/impute-me/statins/SNPs_to_analyze.txt"
-		table<-read.table(table_file,sep="\t",header=T,stringsAsFactors=F)
-		rownames(table)<-table[,"SNP"]
-		genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
-		
-		
-		
-		table[,"Your genotype"]<-genotypes[rownames(table),]
+	  table<-get_data()
+	  
 		table<-table[,c("SNP","Your genotype","effect_allele")]
 		colnames(table)<-c("SNP","Your genotype","Effect allele")
 		table<-table[c("rs3745274","rs2279343"),,drop=FALSE]
 	
 		
 		
-		
-		#write the query to the log file
-		log_function<-function(uniqueID){
-			user_log_file<-paste("/home/ubuntu/data/",uniqueID,"/user_log_file.txt",sep="")
-			m<-c(format(Sys.time(),"%Y-%m-%d-%H-%M-%S"),"drug_response",uniqueID)
-			m<-paste(m,collapse="\t")
-			if(file.exists(user_log_file)){
-				write(m,file=user_log_file,append=TRUE)
-			}else{
-				write(m,file=user_log_file,append=FALSE)
-			}
-		}
-		try(log_function(uniqueID))
+	
 		
 		
 		
