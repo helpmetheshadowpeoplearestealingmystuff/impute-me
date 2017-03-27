0e541a5ef8fbadef7053ec6a1fad45f40458293f
Lasse Folkersen <FOLK@lundbeck.com>
2017-03-27 14:57:25 +0200

    starting ethnicity browser

---
+++ ethnicity/server.R
@@ -0,0 +1,100 @@
+library("shiny")
+library("plotly")
+
+source("/home/ubuntu/srv/impute-me/functions.R")
+
+
+#Replace 'template' with name of module throughout the script
+
+# Define server logic for a template
+shinyServer(function(input, output){
+  output$mainPlot <- renderPlotly({ 
+    if(input$goButton == 0){
+      return(NULL)
+    }
+    uniqueID<-isolate(gsub(" ","",input$uniqueID))
+    if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
+    if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
+    if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
+      Sys.sleep(3) #wait a little to prevent raw-force fishing	
+      stop("Did not find a user with this id")
+    }
+    
+    plot_ly(df, x = rnorm(100,0,10), y = rnorm(100,0,10), z = rnorm(100,0,10), type = "scatter3d", mode = "markers", color=sample(c("red","blue"),100,replace=100))
+  })
+  
+})
+
+
+
+  # dataFile <- isolate(input$dataFile)
+  # headerSelect <- isolate(input$headerSelect)
+  # colourFourth <- isolate(input$colourFourth)
+  # 
+  # 
+  # if(length(headerSelect) != 3)stop("Must select exactly three headers")
+  # 
+  # 
+  # data<-read.csv(paste(data_folder,dataFile,sep=""))
+  # colnames(data) <- standardHeaders
+  # 
+  # 
+  # x<-data[,headerSelect[1]]
+  # y<-data[,headerSelect[2]]
+  # z<-data[,headerSelect[3]]
+  # df <- data.frame(x,y,z)
+  # 
+  # if(colourFourth == "No"){
+  #   plot_ly(df, x = x, y = y, z = z, type = "scatter3d", mode = "markers")
+  #   
+  # }else{
+  #   #if colourFourth is selected we also colour the by remaining axis
+  #   remainingAxis <-standardHeaders[!standardHeaders%in%headerSelect]
+  #   df[,"w"]<-cut(data[,remainingAxis],breaks=4)
+  #   plot_ly(df, x = x, y = y, z = z, type = "scatter3d", mode = "markers", color=df[,"w"],  colors="Blues")
+  # }
+  # 
+  # 
+  # 
+  # })
+  
+  
+  #   output$table1 <- renderTable({ 
+  # 		if(input$goButton == 0){
+  # 			return(NULL)
+  # 		}
+  # 	  uniqueID<-isolate(gsub(" ","",input$uniqueID))
+  # 		if(nchar(uniqueID)!=12)stop("uniqueID must have 12 digits")
+  # 		if(length(grep("^id_",uniqueID))==0)stop("uniqueID must start with 'id_'")
+  # 		if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
+  # 			Sys.sleep(3) #wait a little to prevent raw-force fishing	
+  # 			stop("Did not find a user with this id")
+  # 		}
+  # 
+  # 		
+  # 		
+  # 		
+  # 		table_file <-"../template/SNPs_to_analyze.txt"
+  # 		table<-read.table(table_file,sep="\t",header=T,stringsAsFactors=F)
+  # 		rownames(table)<-table[,"SNP"]
+  # 		#This will return a copy of the SNPs_to_analyze.txt, with the genotypes of this specific person (=uniqueID) as a new column
+  # 		genotypes<-get_genotypes(uniqueID=uniqueID,request=table)
+  # 		table[,"Your genotype"]<-genotypes[rownames(table),]
+  # 		
+  # 		
+  # 		#
+  # 		#
+  # 		#
+  # 		#
+  # 		#
+  # 		#(Optionally) Do any calculations necessary here. Or just return the table as is for showing on web site.
+  # 		#
+  # 		#
+  # 		#
+  # 		#
+  # 		#
+  # 		
+  # 		return(table)
+  # 		
+  # 	},include.rownames = FALSE)
+# 
---
+++ ethnicity/ui.R
@@ -0,0 +1,34 @@
+
+source("../uifunctions.R")
+initialize('sti',TRUE)
+
+shinyUI(bootstrapPage(
+	head(),
+	navigation(),
+	titlePanel("Ethnicity and race"),
+	beginPage(),
+	beginPanel('1/3'),
+	HTML("This browser investigates the ethnicity profile"),
+	
+	textInput(inputId="uniqueID", label = "Unique ID", value = "id_XXXXXXXXX"),
+	actionButton("goButton","Run analysis"),
+	
+	endPanel(),
+	beginPanel('2/3'),
+	
+	h2("Plotly 3D plot of genotype principal components"),
+	
+	endPanel(),
+	
+	# Optional alternative for when you want to show a plot
+	plotlyOutput("mainPlot"),
+	
+	endPage(),
+	footer()
+))
+
+
+
+
+
+
