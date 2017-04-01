06400484d5a0cf43449548e1df8f19a13756e88b
Lasse Folkersen <FOLK@lundbeck.com>
2017-04-01 08:13:43 +0200

    col 7

--- ethnicity/server.R
+++ ethnicity/server.R
@@ -29,13 +29,14 @@ shinyServer(function(input, output){
     z = pc[,"CS3"]
     col <- pc[,"col"]
     # df <- data.frame(x,y,z)
-    plot_ly(pc, x = x, y = y, z = z, type = "scatter3d", mode = "markers", color=col,colors = c("red","blue"),
-            scene = list(
-      xaxis = list(title = "PC1"), 
-      yaxis = list(title = "PC2"), 
-      zaxis = list(title = "PC3"))
-      )
-  
+    plot_ly(pc, x = x, y = y, z = z, type = "scatter3d", mode = "markers", color=col,colors = c("red","blue"))
+    # ,
+    #         scene = list(
+    #   xaxis = list(title = "PC1"), 
+    #   yaxis = list(title = "PC2"), 
+    #   zaxis = list(title = "PC3"))
+    #   )
+    # 
     
     
     })
