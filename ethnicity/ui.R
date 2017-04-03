7c725be2f97bd4ae2c7fde3f3fce0ca88c444738
Lasse Folkersen <FOLK@lundbeck.com>
2017-04-03 16:45:11 +0200

    typo

--- ethnicity/ui.R
+++ ethnicity/ui.R
@@ -14,14 +14,14 @@ shinyUI(bootstrapPage(
 	beginPage(),
 	beginPanel('1/3'),
 	HTML("This browser investigates the ethnicity profile. You can use the ID id_613z86871 to test the function"),
+
 	
-	textInput(inputId="uniqueID", label = "Unique ID", value = "id_613z86871"),
-	# radioButtons("filtering", "filtering", c("None"="None","P<0.05"="0.05","P<0.01"="0.01","P<0.005"="0.005"), selected = "None"),
+	textInput(inputId="uniqueID", label = "Unique ID", value = "id_XXXXXXXXX"),
+	
 	checkboxInput("advanced", label ="Advanced options", value = FALSE),
 	conditionalPanel(
 	  condition = "input.advanced",
 	  checkboxInput("pc_selections", "Principal components:", paste0("PC",as.character(1:5)), selected = c("PC1","PC2","PC3"))
-	  
 	),
 	
 	actionButton("goButton","Run analysis"),
