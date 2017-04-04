9ac6d2afd7161e2506c219b7e3eb8d5052207951
Lasse Folkersen <FOLK@lundbeck.com>
2017-04-04 04:48:00 +0200

    test 2

--- ethnicity/server.R
+++ ethnicity/server.R
@@ -74,8 +74,10 @@ shinyServer(function(input, output){
     pca[,"sizes"]<-c(rep(0.5, nrow(pca)-1),2)
     
     
-    
-    plot_ly(pca, x = paste0("~pos_",pc_selections[1]), y = paste0("~pos_",pc_selections[2]), z = paste0("~pos_",pc_selections[3]), type = "scatter3d", mode = "markers", color= ~pop_long,
+    pca[,"x"]<-paste0("~pos_",pc_selections[1])
+    pca[,"y"]<-paste0("~pos_",pc_selections[2])
+    pca[,"z"]<-paste0("~pos_",pc_selections[3])
+    plot_ly(pca, x = ~x, y = ~y, z = ~z, type = "scatter3d", mode = "markers", color= ~pop_long,
             colors = colours, showlegend=F, hoverinfo = 'name', size = ~sizes, marker = list(symbol = 'circle', sizemode = 'diameter'),
             sizes = c(4, 10)) %>%
       layout(title = 'Genotype-based ethnicity clustering',
