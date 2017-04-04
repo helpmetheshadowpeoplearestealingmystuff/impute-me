63e98d64e5b805f767002e2576bcddee32921700
Lasse Folkersen <FOLK@lundbeck.com>
2017-04-04 04:30:50 +0200

    adding PC-selection

--- ethnicity/server.R
+++ ethnicity/server.R
@@ -1,12 +1,28 @@
 library("shiny")
 library("plotly")
 
+
+
+
+#real
 source("/home/ubuntu/srv/impute-me/functions.R")
-
-
 load("/home/ubuntu/srv/impute-me/ethnicity/2017-04-03_ethnicity_snps.rdata")
 load("/home/ubuntu/srv/impute-me/ethnicity/2017-04-03_ethnicity_pca.rdata")
 ethnicity_desc<-read.table("/home/ubuntu/srv/impute-me/ethnicity/2017-04-03_ethnicity_descriptions.txt",sep="\t",header=T,stringsAsFactors = F,row.names=1)
+
+#local (shiny)
+# source("../functions_local.R")
+# load("2017-04-03_ethnicity_snps.rdata")
+# load("2017-04-03_ethnicity_pca.rdata")
+# ethnicity_desc<-read.table("2017-04-03_ethnicity_descriptions.txt",sep="\t",header=T,stringsAsFactors = F,row.names=1)
+# load("test_genotypes.rdata")
+
+#local
+# source("functions_local.R")
+# load("ethnicity/2017-04-03_ethnicity_snps.rdata")
+# load("ethnicity/2017-04-03_ethnicity_pca.rdata")
+# ethnicity_desc<-read.table("ethnicity/2017-04-03_ethnicity_descriptions.txt",sep="\t",header=T,stringsAsFactors = F,row.names=1)
+# load("ethnicity/test_genotypes.rdata")
 
 
 # Define server logic for a template
@@ -16,34 +32,38 @@ shinyServer(function(input, output){
       return(NULL)
     }
     uniqueID<-isolate(gsub(" ","",input$uniqueID))
-    # pc_selections<-isolate(input$pc_selections)
+    pc_selections<-isolate(input$pc_selections)
     if(nchar(uniqueID)!=12)stop(safeError("uniqueID must have 12 digits"))
     if(length(grep("^id_",uniqueID))==0)stop(safeError("uniqueID must start with 'id_'"))
-    if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
-      Sys.sleep(3) #wait a little to prevent raw-force fishing
-      stop(safeError("Did not find a user with this id"))
+    # if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
+    #   Sys.sleep(3) #wait a little to prevent raw-force fishing
+    #   stop(safeError("Did not find a user with this id"))
+    # }
+    # 
+    if(length(pc_selections)!=3){
+      stop(safeError(paste("For a 3D plot you have to select exactly 3 principal components (PCs), not",length(pc_selections))))
     }
-    
-    # if(sum(pc_selections)!=3){
-    #   stop(safeError(pc_selections))
+
+    #select correct PC's for x y z
+    # for(d in 1:3){
+      # pca_data[,paste0("pos_",c("x","y","z")[d])]<-pca_data[,paste0("pos_PC",d)]
     # }
     
+    
+        
     #get genotypes
-    genotypes<-get_genotypes(uniqueID=uniqueID,request=ethnicity_snps, namingLabel="cached.ethnicity")
+    # genotypes<-get_genotypes(uniqueID=uniqueID,request=ethnicity_snps, namingLabel="cached.ethnicity")
     ethnicity_snps[,"genotype"]<-genotypes[rownames(ethnicity_snps),"genotype"]
     get_alt_count <- function(x){sum(strsplit(x["genotype"],"/")[[1]]%in%x["alt"])}
     ethnicity_snps[,"alt_count"]<-apply(ethnicity_snps,1,get_alt_count)
     
     
     #quick-calculate the PCA metrics for this person
-    pc1<-sum(((ethnicity_snps[,"alt_count"] - ethnicity_snps[,"center"])/ethnicity_snps[,"scale"]) * ethnicity_snps[,"rot_PC1"])
-    pc2<-sum(((ethnicity_snps[,"alt_count"] - ethnicity_snps[,"center"])/ethnicity_snps[,"scale"]) * ethnicity_snps[,"rot_PC2"])
-    pc3<-sum(((ethnicity_snps[,"alt_count"] - ethnicity_snps[,"center"])/ethnicity_snps[,"scale"]) * ethnicity_snps[,"rot_PC3"])
-    pc4<-sum(((ethnicity_snps[,"alt_count"] - ethnicity_snps[,"center"])/ethnicity_snps[,"scale"]) * ethnicity_snps[,"rot_PC4"])
-    pc5<-sum(((ethnicity_snps[,"alt_count"] - ethnicity_snps[,"center"])/ethnicity_snps[,"scale"]) * ethnicity_snps[,"rot_PC5"])
-    
-    
-    you<-data.frame(pop="YOU", super_pop="YOU", gender=NA,   pos_PC1=pc1,  pos_PC2=pc2,   pos_PC3=pc3,  pos_PC4=pc4 ,    pos_PC5=pc5,stringsAsFactors = F)
+    you<-data.frame(pop="YOU", super_pop="YOU", gender=NA,stringsAsFactors = F)
+    for(pc in 1:5){
+      val<-sum(((ethnicity_snps[,"alt_count"] - ethnicity_snps[,"center"])/ethnicity_snps[,"scale"]) * ethnicity_snps[,paste0("rot_PC",pc)])
+      you[,paste0("pos_PC",pc)]<-val
+    }
     pca<-rbind(pca_data,you)
     
     
@@ -60,17 +80,17 @@ shinyServer(function(input, output){
     
     
     
-    plot_ly(pca, x = ~pos_PC1, y = ~pos_PC2, z = ~pos_PC3, type = "scatter3d", mode = "markers", color= ~pop_long,
+    plot_ly(pca, x = paste0("~pos_",pc_selections[1]), y = paste0("~pos_",pc_selections[2]), z = paste0("~pos_",pc_selections[3]), type = "scatter3d", mode = "markers", color= ~pop_long,
             colors = colours, showlegend=F, hoverinfo = 'name', size = ~sizes, marker = list(symbol = 'circle', sizemode = 'diameter'),
             sizes = c(4, 10)) %>%
       layout(title = 'Genotype-based ethnicity clustering',
-             scene = list(xaxis = list(title = 'PC1',
+             scene = list(xaxis = list(title = pc_selections[1],
                                        gridcolor = 'rgb(255, 255, 255)',
                                        gridwidth = 2),
-                          yaxis = list(title = 'PC2',
+                          yaxis = list(title = pc_selections[2],
                                        gridcolor = 'rgb(255, 255, 255)',
                                        gridwith = 2),
-                          zaxis = list(title = 'PC3',
+                          zaxis = list(title = pc_selections[3],
                                        gridcolor = 'rgb(255, 255, 255)',
                                        gridwith = 2)),
              paper_bgcolor = 'rgb(243, 243, 243)',
