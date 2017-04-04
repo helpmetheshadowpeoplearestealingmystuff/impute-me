70f1ceb2692aebe501a271310c918d14b500edbd
Lasse Folkersen <FOLK@lundbeck.com>
2017-04-04 04:43:52 +0200

    test 4h

--- ethnicity/server.R
+++ ethnicity/server.R
@@ -1,8 +1,8 @@
 library("shiny")
 library("plotly")
-options(shiny.error = browser)
+# options(shiny.error = browser)
 
-
+options(shiny.sanitize.errors=F)
 
 #real
 source("/home/ubuntu/srv/impute-me/functions.R")
