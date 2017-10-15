d5ab25a99cd01b8d619e7736a8785a9dfc877b68
Lasse Folkersen <FOLK@lundbeck.com>
2017-10-15 15:39:27 +0200

    spelling error

--- kandinsky/ui.R
+++ kandinsky/ui.R
@@ -18,7 +18,7 @@ shinyUI(bootstrapPage(
 	
 	
 	endPanel(),
-	HTML("Making unique art from your genomic data is not a new idea. However, most places I've seen that offer such service actually just use very few SNPs. In this module the all SNPs analysed in the impute.me setting is combined to create a truly unique piece of art in the style of <u><a href='https://en.wikipedia.org/wiki/Wassily_Kandinsky'>Wassily Kandinsky</a></u>, using the beautiful code available in the <i><u><a href='http://giorasimchoni.com/2017/07/30/2017-07-30-data-paintings-the-kandinsky-package/'>kandinsky</a></u></i> R-package.<br><br><br>
+	HTML("Making unique art from your genomic data is not a new idea. However, most places I've seen that offer such service actually just use very few SNPs. In this module all your trait-associated SNPs are combined to create a truly unique piece of art in the style of <u><a href='https://en.wikipedia.org/wiki/Wassily_Kandinsky'>Wassily Kandinsky</a></u>, using the beautiful code available in the <i><u><a href='http://giorasimchoni.com/2017/07/30/2017-07-30-data-paintings-the-kandinsky-package/'>kandinsky</a></u></i> R-package.<br><br><br>
 	     You may ask if the drawing reveals deeper insight about your genome and inner self? Maybe it does, maybe it doesn't. Either way - it is guaranteed to be a unique drawing derived only from your genome. You may print it and use it as art. I do.
 	     "),
 	plotOutput("plot_1"),
