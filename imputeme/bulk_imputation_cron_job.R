fba9365fccd5e03fa8fe7459ac0c201d5a92b78d
Lasse Folkersen <FOLK@lundbeck.com>
2017-11-07 08:17:32 +0100

    don't do bulk imputation runs with fast-queue stuff

--- imputeme/bulk_imputation_cron_job.R
+++ imputeme/bulk_imputation_cron_job.R
@@ -51,11 +51,10 @@ if(serverRole== "Node"){
   remoteFoldersToCheck<-remotedata_df[,7]
   
   
-  #check if there's any fast-queue jobs to put up-front
-#   cmd0 <- paste("ssh ubuntu@",hubAddress," cat /home/ubuntu/misc_files/fast_queue_emails.txt
-# ",sep="")
-#   f<-system(cmd0,intern=T)
-#   remoteFoldersToCheck<-c(remoteFoldersToCheck[remoteFoldersToCheck%in%f],remoteFoldersToCheck[!remoteFoldersToCheck%in%f])
+  #check if there's any fast-queue jobs - and remove them! They don't go into bulk imputation
+  cmd0 <- paste("ssh ubuntu@",hubAddress," cat /home/ubuntu/misc_files/fast_queue_emails.txt",sep="")
+  f<-system(cmd0,intern=T)
+  remoteFoldersToCheck<-c(remoteFoldersToCheck[!remoteFoldersToCheck%in%f])
   
   
   #then loop over all remote folders - first checking if there is 'length_requested'  ready jobs
