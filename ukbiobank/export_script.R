91eca36dc19277b6e45e99ea0a24e25e510dacee
Lasse Folkersen <FOLK@lundbeck.com>
2018-01-25 11:19:17 +0100

    having the export script also normalize by ethnicity

--- ukbiobank/export_script.R
+++ ukbiobank/export_script.R
@@ -19,15 +19,32 @@ export_function<-function(uniqueID){
   output<-list()
   
   
+  #get ethnicity parameter
+  pDataFile<-paste("/home/ubuntu/data/",uniqueID,"/pData.txt",sep="")
+  pData<-try(read.table(pDataFile,header=T,stringsAsFactors=F,sep="\t"))
+  if(class(pData)!="try-error" && "ethnicity" %in% colnames(pData)){
+    ethnicity <-pData[1,"ethnicity"]
+  }else{
+    ethnicity <-"global"
+  }
+  
+  
   for(study_id in rownames(traits)){
     
     SNPs_to_analyze<-data[data[,"study_id"]%in%study_id ,]
     
-    
+    #get genotypes
     SNPs_requested<-SNPs_to_analyze[!duplicated(SNPs_to_analyze[,"SNP"]),]
     rownames(SNPs_requested)<-SNPs_requested[,"SNP"]
     genotypes<-get_genotypes(uniqueID=uniqueID,request=SNPs_requested, namingLabel="cached.all_gwas")
     
+    
+    #get correct ethnicity minor_allele_frequency
+    if(ethnicity %in% c("EAS","AMR","AFR","EUR","SAS")){
+      SNPs_requested[,"minor_allele_freq"]<-SNPs_requested[,paste0(ethnicity,"_AF")]
+    }
+    
+    #calculate GRS
     snp_data<-SNPs_requested
     snp_data[,"genotype"] <- genotypes[rownames(snp_data),"genotype"]
     snp_data <-get_GRS_2(snp_data,mean_scale=T, unit_variance=T, verbose=T)
