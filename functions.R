aa9a17000a56613d9e301780d47cb43ab56232d7
Lasse Folkersen <FOLK@lundbeck.com>
2018-02-09 09:18:38 +0100

    minor thing

--- functions.R
+++ functions.R
@@ -1600,11 +1600,13 @@ generate_report<-function(uniqueIDs=NULL, filename=NULL){
 
 
 run_export_script<-function(uniqueIDs=NULL,modules=NULL, delay=0){
-  require(jsonlite)
   #A function that will crawl all module directories and execute the export script if present
   #uniqueID:    Indicates if specific sets should be processed
   #modules:     Indicates if specific modules should be procssed
   #delay:       an integer, in seconds, giving an optional delay to insert after each uniqueID
+  
+  require(jsonlite)
+  require(shiny) #call safeError, but often will be run from cmd-line
   
   if(class(delay)!="numeric")stop(paste("delay must be numeric, not",class(delay)))
   if(length(delay)!=1)stop(paste("delay must be lengh 1, not",length(delay)))
