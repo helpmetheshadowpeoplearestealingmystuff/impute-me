68feda8893199e49a8466bd172c23f3e1e6c58b8
Lasse Folkersen <FOLK@lundbeck.com>
2018-03-19 12:19:33 +0100

    Getting the intelligence export script running (still a bit sceptical to results, not ready for opening yet)

---
+++ intelligence/export_script.R
@@ -0,0 +1,67 @@
+source("/home/ubuntu/srv/impute-me/functions.R")
+
+
+export_function<-function(uniqueID){
+  snps_file<-"/home/ubuntu/srv/impute-me/intelligence/SNPs_to_analyze.txt"
+  SNPs_to_analyze_all <- read.table(snps_file,sep="\t",stringsAsFactors = F,header=T)
+  
+  
+  if(!file.exists(paste("/home/ubuntu/data/",uniqueID,sep=""))){
+    stop("Did not find a user with this id")
+  }
+  
+  
+  
+  study_ids <- c("intelligence_29326435","emotional_Intelligence_29527006")
+  names(study_ids) <- c("IQ","EQ")
+
+  #get ethnicity parameter
+  pDataFile<-paste("/home/ubuntu/data/",uniqueID,"/pData.txt",sep="")
+  pData<-try(read.table(pDataFile,header=T,stringsAsFactors=F,sep="\t"))
+  if(class(pData)!="try-error" && "ethnicity" %in% colnames(pData)){
+    ethnicity <-pData[1,"ethnicity"]
+  }else{
+    ethnicity <-"global"
+  }
+  
+  
+  
+  output<-list()
+
+  for(study_id in study_ids){
+    SNPs_to_analyze<-SNPs_to_analyze_all[SNPs_to_analyze_all[,"study_id"]%in%study_id ,]
+    
+  
+    #get genotypes
+    SNPs_requested<-SNPs_to_analyze[!duplicated(SNPs_to_analyze[,"SNP"]),]
+    rownames(SNPs_requested)<-SNPs_requested[,"SNP"]
+    genotypes<-get_genotypes(uniqueID=uniqueID,request=SNPs_requested, namingLabel="cached.all_gwas")
+    
+    #get correct ethnicity minor_allele_frequency
+    if(ethnicity %in% c("EAS","AMR","AFR","EUR","SAS")){
+      SNPs_requested[,"minor_allele_freq"]<-SNPs_requested[,paste0(ethnicity,"_AF")]
+    }
+    
+    
+    #calculate GRS
+    snp_data<-SNPs_requested
+    snp_data[,"genotype"] <- genotypes[rownames(snp_data),"genotype"]
+    snp_data <-get_GRS_2(snp_data,mean_scale=T, unit_variance=T, verbose=T)
+    population_sum_sd<-sqrt(sum(snp_data[,"population_score_sd"]^2,na.rm=T))
+    GRS_beta <-sum(snp_data[,"score_diff"],na.rm=T) / population_sum_sd
+    
+    output[[study_id]]<- GRS_beta
+    
+           
+           
+  }
+  
+  
+  
+  return(output)
+  
+}
+
+
+
+
