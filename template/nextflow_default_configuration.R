90fd0d03dc0f39b6f48f10d1f1e18cd56aec2d08
Lasse Folkersen <lwf@ngc.dk>
2022-01-02 21:22:09 +0100

    - Updating license - Update nextflow default configuration

--- template/nextflow_default_configuration.R
+++ template/nextflow_default_configuration.R
@@ -3,4 +3,5 @@ vcfs_path <- "./"
 autostart_supercronic <- FALSE
 minimum_required_variant_in_vcf_count <- 1000
 modules_to_compute <- "ethnicity,AllDiseases"
-verbose <- 3
+verbose <- 1
+submission_logs_path <- "./"
